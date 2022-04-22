9689bc0d542a0fbca2060efb823730ea90e4e4cd
edomingues <edi.carlos.do.ec@gmail.com>
2022-04-22 13:41:28 -0300

    ajusted fun of the view model

--- app/src/main/java/com/example/weather/search/SearchViewModel.kt
+++ app/src/main/java/com/example/weather/search/SearchViewModel.kt
@@ -3,6 +3,7 @@ package com.example.weather.search
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.domain.weather.base.Event
+import com.example.domain.weather.search.model.Weather
 import com.example.domain.weather.search.usecase.GetWeatherUseCase
 import com.example.domain.weather.util.ISchedulerProvider
 import com.example.weather.base.BaseViewModel
@@ -22,6 +23,10 @@ class SearchViewModel(
     val loading: LiveData<Boolean?>
         get() = _loading
 
+    private val _weather = MutableLiveData<Weather?>()
+    val weather: LiveData<Weather?>
+        get() = _weather
+
     fun getWeather() {
         getWeatherUseCase.execute()
             .subscribeOn(scheduler.backgroundThread())
@@ -30,10 +35,10 @@ class SearchViewModel(
                 _loading.value = it.isLoading()
                 when (it) {
                     is Event.Data<*> -> {
-
+                        _weather.value = it.data as Weather
                     }
                     is Event.Error -> {
-
+                        _error.value = it
                     }
                     else -> Unit
                 }
