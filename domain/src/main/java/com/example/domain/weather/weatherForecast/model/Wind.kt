18c311863395b5931f532d9986dd2c9d15387132
Edi <edi.carlos.do.ec@gmail.com>
2022-06-18 17:54:06 -0300

    :art: changed parcelize imports

--- domain/src/main/java/com/example/domain/weather/weatherForecast/model/Wind.kt
+++ domain/src/main/java/com/example/domain/weather/weatherForecast/model/Wind.kt
@@ -1,7 +1,7 @@
 package com.example.domain.weather.weatherForecast.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 import java.math.BigDecimal
 
 @Parcelize
