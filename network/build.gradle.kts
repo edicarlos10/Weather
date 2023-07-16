a05c01501a2aea2eff8e48eb561301a4a5ab1252
edicarlos10 <edi.carlos.do.ec@gmail.com>
2023-07-16 12:42:56 -0300

    :art: migrate gradle network to catalogs

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -36,20 +36,19 @@ android {
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation (project(path = ":domain"))
-    implementation ("androidx.core:core-ktx:1.8.0")
-    implementation ("androidx.appcompat:appcompat:1.4.2")
-    implementation ("com.google.android.material:material:1.6.1")
-    testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+    implementation (libs.androidx.ktx)
+    implementation (libs.androidx.appcompat)
+    implementation (libs.android.material)
+    testImplementation (libs.junit)
 
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
-    implementation ("com.squareup.okhttp3:okhttp:5.0.0-alpha.6")
-    implementation ("com.squareup.moshi:moshi-kotlin:1.12.0")
+    //retrofit
+    implementation (libs.squareup.retrofit2)
+    implementation (libs.squareup.retrofit2.gson)
+    implementation (libs.squareup.retrofit2.moshi)
+    implementation (libs.squareup.okhttp3)
 
-    implementation ("io.reactivex.rxjava2:rxjava:2.2.21")
-    implementation ("io.reactivex.rxjava2:rxkotlin:2.4.0")
-    implementation ("io.reactivex.rxjava2:rxandroid:2.1.1")
+    //rxjava
+    implementation (libs.reactivex.rxjava)
+    implementation (libs.reactivex.rxjava.rxkotlin)
+    implementation (libs.reactivex.rxjava.rxandroid)
 }
