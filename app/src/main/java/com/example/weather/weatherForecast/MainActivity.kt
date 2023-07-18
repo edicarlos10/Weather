b64bd4d4e9bebfef1415331f3809f1ff717aef82
edicarlos10 <edi.carlos.do.ec@gmail.com>
2023-07-18 12:00:41 -0300

    :fire: removed key

--- app/src/main/java/com/example/weather/weatherForecast/MainActivity.kt
+++ app/src/main/java/com/example/weather/weatherForecast/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HomeScreen(weatherForecastViewModel, "58611e500e6bfe3dd41851e99a88a932")
+                    HomeScreen(weatherForecastViewModel, "")
                 }
             }
         }
