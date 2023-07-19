15a5b731f5f281d9bd56c3de5311678317295593
edicarlos10 <edi.carlos.do.ec@gmail.com>
2023-07-19 11:46:15 -0300

    :art: hide keyboard on success

--- app/src/main/java/com/example/weather/weatherForecast/home/HomeScreen.kt
+++ app/src/main/java/com/example/weather/weatherForecast/home/HomeScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.domain.weather.base.Event
@@ -42,6 +43,7 @@ fun Body(weatherForecastViewModel: WeatherForecastViewModel?) {
     weatherState?.value?.let { state ->
         when (state) {
             is Event.Data -> {
+                LocalFocusManager.current.clearFocus()
                 CardContent(state.data)
             }
 
