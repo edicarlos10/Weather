97ee69b0f8eb666fb13bfd79a49bebbe64909139
edicarlos10 <edi.carlos.do.ec@gmail.com>
2023-07-19 09:10:44 -0300

    :art: added glide to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,4 +96,7 @@ dependencies {
     implementation(libs.compose.ui.graphics)
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.material3)
+
+    // Glide
+    implementation (libs.glide)
 }
